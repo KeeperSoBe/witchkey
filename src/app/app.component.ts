17ff038cabdf67df84b0005c79eb173d03201a72
James <KeeperSoBe@gmail.com>
2024-11-28 23:30:47 +0000

    feat: create and implement navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,95 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+interface KeyEventInterface {
+  key: string;
+  location: string;
+  code: string;
+  keyCode?: number;
+}
+
+const KeyEventLocation: { [key: number]: string } = {
+  0: '(General keys)',
+  1: '(Left-side modifier keys)',
+  2: '(Right-side modifier keys)',
+  3: '(Numpad)',
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'witchkey';
+export class AppComponent implements OnInit, OnDestroy {
+  public readonly allKeyCodes: KeyEventInterface[] = [];
+
+  public isDarkMode = true;
+  public isShowingKeycodeTable = false;
+
+  public constructor(
+    @Inject(DOCUMENT)
+    private readonly document: Document,
+  ) {}
+
+  public ngOnInit(): void {
+    this.document.addEventListener('keydown', (event) =>
+      this.keydownHandler(event),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.document.removeEventListener('keydown', (event) =>
+      this.keydownHandler(event),
+    );
+  }
+
+  public toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  public toggleShowKeycodeTable(): void {
+    this.isShowingKeycodeTable = !this.isShowingKeycodeTable;
+  }
+
+  private keydownHandler({
+    code,
+    key,
+    location,
+    keyCode,
+  }: KeyboardEvent): void {
+    let indexOfKeyCode = -1;
+
+    for (let index = 0; index < this.allKeyCodes.length; index++) {
+      const item = this.allKeyCodes[index];
+
+      if (item.key === key && item.code === code) {
+        indexOfKeyCode = index;
+        break;
+      }
+    }
+
+    if (indexOfKeyCode !== -1) {
+      this.allKeyCodes.splice(indexOfKeyCode, 1);
+    }
+
+    this.allKeyCodes.push({
+      code,
+      keyCode,
+      key: key === ' ' && code === 'Space' ? '(Spacebar)' : key,
+      location: `${location} ${KeyEventLocation[location]}`,
+    });
+  }
+
+  // public copyToClipboard(message: string): void {
+  //   navigator.clipboard.writeText(message).then(
+  //     () => {
+  //       const toaster = this.$refs.toaster as Toaster;
+  //       toaster.createToast('Copied to clipboard');
+  //     },
+  //     (err) => console.error('Async: Could not copy text: ', err),
+  //   );
+  // }
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  @Input({ required: true })
+  public darkMode = true;
+
+  @Input({ required: true })
+  public showingKeycodeTable = false;
+
+  @Output()
+  public readonly toggleDarkMode: EventEmitter<void> = new EventEmitter<void>();
+
+  @Output()
+  public readonly toggleShowKeycodeTable: EventEmitter<void> =
+    new EventEmitter<void>();
+}
